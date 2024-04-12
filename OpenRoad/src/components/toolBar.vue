425b01a70b667e9e6c62022bd0c31203881d973f
Sid.Daniel <sidneylopes17@gmail.com>
2024-04-11 23:18:15 -0300

    first commit

---
+++ OpenRoad/src/components/toolBar.vue
@@ -0,0 +1,31 @@
+<script lang="js">
+import Avatar from 'primevue/avatar';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Menubar from 'primevue/menubar';
+
+export default {
+  name: 'toolBar',
+  components:{
+    Menubar, Avatar, InputText, Button
+  } 
+}
+
+</script>
+
+<template>
+
+<div class="card" style="width: 100%;">
+        <Toolbar style="border-radius: 3rem; padding: 0.2rem 1rem 0.2rem 3rem;">
+            <template #start>
+                <div class="flex align-items-center gap-2" style="display: flex; gap: 1.5rem;">
+                    <Avatar image="/src/assets/motorcycles.svg" class="mr-2" size="large" shape="circle" />
+                    <Button label="Files" text plain />
+                    <Button label="Edit" text plain />
+                    <Button label="View" text plain />
+                </div>
+            </template>
+
+        </Toolbar>
+    </div>
+</template>
---
+++ OpenRoad/src/views/HelloSection.vue
@@ -0,0 +1,18 @@
+<script setup>
+import toolBar from '@/components/toolBar.vue';
+</script>
+
+<template>
+  <main>
+    <toolBar/>
+  </main>
+</template>
+
+<style scoped>
+/* main{
+
+  min-height: 100vh;
+} */
+
+
+</style>
